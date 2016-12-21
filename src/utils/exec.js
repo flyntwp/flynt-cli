ee3aa83740da15892b8e50a8a25f1ff2d1d8ad9c
Dominik Tränklein <dominik@bleech.de>
2016-12-21 14:25:33 +0100

    feat(exec): use spawn instead of exec and include fix for windows

--- src/utils/exec.js
+++ src/utils/exec.js
@@ -3,10 +3,23 @@ import Promise from 'bluebird'
 
 export default function (cmds) {
   return new Promise(function (resolve, reject) {
-    let exec = childProcess.exec(cmds.join(' && '), function () {
+    let spawn
+
+    const isWin = process.platform === 'win32'
+    if (isWin) {
+      spawn = childProcess.spawn(
+        'cmd.exe',
+        ['/s', '/c', '"' + cmds.join(' && ') + '"'],
+        { windowsVerbatimArguments: true, stdio: [process.stdin, 'pipe', 'pipe'] }
+      )
+    } else {
+      spawn = childProcess.spawn('/bin/sh', ['-c', cmds.join(' && ')])
+    }
+
+    spawn.stdout.pipe(process.stdout)
+    spawn.stderr.pipe(process.stderr)
+    spawn.on('exit', function () {
       resolve()
     })
-    exec.stdout.pipe(process.stdout)
-    exec.stderr.pipe(process.stderr)
   })
 }
