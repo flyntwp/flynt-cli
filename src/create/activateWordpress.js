972a9bdf24bfa3b5085cfe16eff32e89483c526d
Dominik Tränklein <dominik@bleech.de>
2017-02-27 15:34:30 +0100

    fix(activateWordpress): set default rewrite structure

--- src/create/activateWordpress.js
+++ src/create/activateWordpress.js
@@ -16,7 +16,7 @@ export function run (answers) {
     'wp option set blog_public 0',
     `wp theme activate ${answers.themeName}`,
     'echo "apache_modules:\n  - mod_rewrite\n" >> wp-cli.yml',
-    'wp rewrite flush --hard',
+    'wp rewrite structure /%postname%/ --hard',
     'wp plugin activate --all'
   ]
   return exec(cmds)
