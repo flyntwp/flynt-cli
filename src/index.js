d1204196b6d67119882d322d0cc6c6295fb2410b
Dominik Tränklein <dominik@bleech.de>
2017-07-07 13:16:09 +0200

    feat(verboseMode): default to true

--- src/index.js
+++ src/index.js
@@ -59,7 +59,8 @@ cli.option('skipReadConfig', {
   alias: 'verbose',
   global: true,
   describe: 'Use verbose mode',
-  type: 'boolean'
+  type: 'boolean',
+  default: true
 })
 .version()
 .help()
